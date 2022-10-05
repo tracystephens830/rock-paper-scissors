b6b3f1394eef8ab217c931584d532347664a1bca
tracystephens830 <tracystephens830@gmail.com>
2022-10-04 20:00:50 -0400

    Write pseudocode comments into rock-paper-scissors.js

---
+++ rock-paper-scissors.js
@@ -0,0 +1,15 @@
+//Assign number 1, 2, and 3 a value of rock, paper, and scissor
+//Ask user to choose rock, paper, or scissor
+//Store input in variable
+//Randomly generate a number variable that is <=1 and >=3
+//Compare user input to random number
+//Console.log result of comparison
+//Assign output array
+//Run chooseWeapon function again
+//Console.log result of comparison
+//Assign output to array
+//IF array[0] output == 2, you win ELSEIF array [1] == 2, you lose, ELSE run chooseWeapon again
+//Run chooseWeapon function again
+//Assign output to array
+//IF array[0] == 2, you win best 2/3 ELSE you lose!
+//console.log result
